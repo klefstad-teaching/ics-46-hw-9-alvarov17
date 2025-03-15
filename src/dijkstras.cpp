bebadd0103eee2756bd197109524a96ab9d739ab
Alvaro Ventura <alvarov2@uci.edu>
2025-03-15 00:14:09 -0700

    dijkstra's is done FINALLYYYY

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -5,21 +5,27 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
     int numVertices = G.size();
     vector<int> distance(numVertices, INF);
     vector<bool> visited(numVertices, false);
+
     distance[source] = 0;
     fill(previous.begin(), previous.end(), -1);
+    previous[source] = -1;
+
     priority_queue< pair<int, int>, vector<pair<int, int>>, MyComparator > minHeap;
     minHeap.push({0, source});
+
     while (!minHeap.empty())
     {
         int u = minHeap.top().second;
         minHeap.pop();
+
         if (visited[u]) continue;
         visited[u] = true;
+
         for (Edge e : G[u])
         {
             int v = e.dst;
             int weight = e.weight;
-            if (!visited[v] && distance[u] + weight < distance[v])
+            if (!visited[v] && (distance[u] + weight < distance[v]))
             {
                 distance[v] = distance[u] + weight;
                 previous[v] = u;
@@ -33,11 +39,9 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
 vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination)
 {
     stack<int> path;
-    int v = destination;
-    while (v != -1)
+    for (; destination != -1; destination = previous[destination])
     {
-        path.push(v);
-        v = previous[v];
+        path.push(destination);
     }
     vector<int> shortest_path;
     while (!path.empty())
--- src/dijkstras_main.cpp
+++ src/dijkstras_main.cpp
@@ -2,11 +2,12 @@
 
 int main()
 {
+    int dest = 3;
     Graph g;
-    file_to_graph("src/small.txt", g);
+    file_to_graph("small.txt", g);
     vector<int> prev{g.numVertices, -1};
-    vector<int> path = dijkstra_shortest_path(g, 0, prev);
-    vector<int> short_path = extract_shortest_path(prev, path, 0);
-    print_path(short_path, 0);
+    vector<int> distances = dijkstra_shortest_path(g, 0, prev);
+    vector<int> short_path = extract_shortest_path(distances, prev, dest);
+    print_path(short_path, distances[dest]);
     return 0;
 }
