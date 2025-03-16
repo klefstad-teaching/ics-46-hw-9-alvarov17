27bd4710bb25213aca571c11b9adcfd105e81e93
Alvaro Ventura <alvaroventura17@gmail.com>
2025-03-16 02:56:46 +0000

    changed dijkstra

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -1,41 +1,75 @@
 #include "dijkstras.h"
 
+// vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous)
+// {
+//     int numVertices = G.size();
+//     vector<int> distance(numVertices, INF);
+//     vector<bool> visited(numVertices, false);
+
+//     distance[source] = 0;
+//     fill(previous.begin(), previous.end(), -1);
+//     previous[source] = -1;
+
+//     priority_queue< pair<int, int>, vector<pair<int, int>>, MyComparator > minHeap;
+//     minHeap.push({0, source});
+
+//     while (!minHeap.empty())
+//     {
+//         int u = minHeap.top().second;
+//         minHeap.pop();
+
+//         if (visited[u]) continue;
+//         visited[u] = true;
+
+//         for (Edge e : G[u])
+//         {
+//             int v = e.dst;
+//             int weight = e.weight;
+//             if (!visited[v] && (distance[u] + weight < distance[v]))
+//             // if ((distance[u] + weight < distance[v]))
+//             {
+//                 distance[v] = distance[u] + weight;
+//                 previous[v] = u;
+//                 minHeap.push({distance[v], v});
+//             }
+//         }
+//     }
+//     return distance;
+// }
+
+
 vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous)
 {
     int numVertices = G.size();
     vector<int> distance(numVertices, INF);
     vector<bool> visited(numVertices, false);
-
-    distance[source] = 0;
     fill(previous.begin(), previous.end(), -1);
-    previous[source] = -1;
-
     priority_queue< pair<int, int>, vector<pair<int, int>>, MyComparator > minHeap;
     minHeap.push({0, source});
+    distance[source] = 0;
 
     while (!minHeap.empty())
     {
-        int u = minHeap.top().second;
+        int current = minHeap.top().second;
         minHeap.pop();
 
-        if (visited[u]) continue;
-        visited[u] = true;
-
-        for (Edge e : G[u])
+        if (visited[current]) continue;
+        visited[current] = true;
+        for (Edge e : G[current])
         {
             int v = e.dst;
             int weight = e.weight;
-            // if (!visited[v] && (distance[u] + weight < distance[v]))
-            if ((distance[u] + weight < distance[v]))
+            if (!visited[v] && distance[current] + weight < distance[v])
             {
-                distance[v] = distance[u] + weight;
-                previous[v] = u;
+                distance[v] = distance[current] + weight;
+                previous[v] = current;
                 minHeap.push({distance[v], v});
             }
         }
     }
     return distance;
 }
+
 
 vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination)
 {
--- src/dijkstras_main.cpp
+++ src/dijkstras_main.cpp
@@ -2,12 +2,13 @@
 
 int main()
 {
-    int dest = 3;
     Graph g;
-    file_to_graph("small.txt", g);
+    file_to_graph("medium.txt", g);
     vector<int> prev{g.numVertices, -1};
     vector<int> distances = dijkstra_shortest_path(g, 0, prev);
-    vector<int> short_path = extract_shortest_path(distances, prev, dest);
-    print_path(short_path, distances[dest]);
+    for (int i = 0; i < 6 ; ++i) {
+        vector<int> short_path = extract_shortest_path(distances, prev, i);
+        print_path(short_path, distances[i]);
+    }
     return 0;
 }
